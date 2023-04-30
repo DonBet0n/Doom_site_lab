969a2482a6c7bd32edd3f9b13b029a11627891e4
DonBet0n <mr.kostushevich@gmail.com>
2023-04-30 18:57:41 +0300

    site 1.0

---
+++ index.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css\style.css" rel="stylesheet">
+    <link type="image/x-icon" href="img\doomicon512.ico" rel="shortcut icon">
+    <title>Doom In</title>
+</head>
+<body>
+    <header id="start">
+        <div class="header__wrapper">
+        <img class="bck" src="img\bck_description.png" >
+            <div class="line_top" id="topline">
+                <div class="logo"><img src="img\logo.svg" alt=""></div>
+                <div class="name_site">Doom</div>
+                <div class="profile"><a href="#"><img src="img\profile.svg"></a></div>
+            </div>
+            <div class="header__flexblock-column">
+                <div class="flexblock">
+                    <ul>
+                        <li class="li_flexblock"><a href="#start">Вступ</a><div class="line"></div></li>
+                        <li class="li_flexblock"><a href="#tab_description">опис</a><div class="line"></div></li>
+                        <li class="li_flexblock"><a href="#tab_kat_roka">Кат року</a><div class="line"></div></li>
+                        <li class="li_flexblock"><a href="#tab_demons">демони</a><div class="line"></div></li>
+                        <li class="li_flexblock"><a href="#tab_guns">зброя</a><div class="line"></div></li>
+                        <li class="li_flexblock"><a href="https://store.steampowered.com/app/782330/DOOM_Eternal/">сторiнка В STEAM</a><div class="line"></div></li>
+                    </ul>
+                </div>
+            </div>
+
+        <div class="evaluations"> 
+                    
+            <div class="title_evaluations">doom eternal</div>  
+                <div class="score">
+                    <div class="score1"><img src="img\square_88.svg"><p>оцінка критиків</p></div>
+                    <div class="score1"><img src="img\circle8.8.svg"><p>оцінка гарвців</p></div>
+                 </div>
+            <div class="commit">
+                <div class="commit__name">
+                        <img class="img95" src="img\square_95.svg">
+                    <div class="men">
+                        <div class="nickname">gamespace</div>
+                        <div class="date">27.04.2020</div>
+                    </div> 
+                </div>
+            <div class="commit_text">Якщо вам сподобався DOOM 2016, думаю, 
+                вам сподобається DOOM Eternal. Вона взяла найкращі моменти 
+                2016 року і якимось чином знайшла спосіб їх покращити. 
+                Додайте сюди дивовижні пасхальні яйця, нові типи ворогів, 
+                механіку руху та дизайн рівнів - і ви отримаєте формулу 
+                дивовижності. Звичайно, вам знадобиться рівень або близько 
+                того, щоб зрозуміти цей новий бойовий ритм, але як тільки 
+                ви його знайдете, ви ніколи не захочете повертатися назад.
+            </div>
+            </div>
+        </div>
+            
+                     
+
+
+        </div>
+</header> 
+
+        <!-- MAIN -->
+
+<main>
+
+    <!-- START -->
+
+    <div class="main__wrapper">
+        <div class="wrapper">
+            <div id="tab_description" class="tab_description">  
+                <div class="description_content">
+                       
+                    <div class="content_text_wrapper">
+                        <div class="black"></div>
+                                        <video class="video1" nocontrols autoplay muted loop>
+                                            <source src="video\doom_1.mp4" type="video/mp4">
+                                        </video>
+                            <div class="content">
+                                <h2 class="title">Doom eternal</h2>
+                                    <div class="content_text">
+                                        гра в жанрі шутер від першої особи,
+                                        розроблена компанією Id Software і є сіквелом DOOM.
+                                        Протистояння Кату Року і сил пекла триває,
+                                        тепер вам доведеться звільнити Землю від 
+                                        демонічної загрози, переміщатися за вимірами
+                                        та знищувати як нових істот у всесвіті Дум,
+                                        так і добре знайомих демонів із потужною
+                                        зброєю та здібностями.
+                                    </div>
+                            </div>
+                    </div>
+                </div> 
+            </div>
+        </div>
+
+        <div class="clearfix"></div> 
+            
+
+            <!-- KAT ROKA -->
+
+            <div class="wrapper">
+            <div id="tab_kat_roka" class="tab_description">  
+                    <div class="description_content">
+                       
+                        <div class="content_text_wrapper">
+                                <div class="black"></div>
+                                        <video class="video1" nocontrols autoplay muted loop>
+                                            <source src="video\doom_2.mp4" type="video/mp4">
+                                        </video>
+                                <div class="content">
+                                        <h2 class="title">1. Кат рока</h2>
+                                        <div class="content_text" style="font-size: 30px;" >
+                                            колишній космічний десантник, який став великим 
+                                            воїном Нічних Стражів. Він почав свій хрестовий
+                                            похід проти демонів після того, як вони напали
+                                            на Фобос і Деймос де він служив.
+                                            Він пройшов через Ад, знищуючи всіх демонів на
+                                            своєму шляху, і потрапив у вимір Аргент Д’Нур,
+                                            де він знайшов нових союзників і нову броню.
+                                            Він був запечатаний у саркофазі демонами,
+                                            але потім був звільнений ОАК на Марсі.
+                                            Він продовжив свою боротьбу з демонами 
+                                            на Марсі, Землі і в інших вимірах.
+                                            Він має надзвичайно високий рівень
+                                            фізичної сили, швидкості, витривалості
+                                            і інтелекту, а також доступ до розмаїття
+                                            потужної зброї і технологій
+                                        </div>
+                                </div>
+                        </div>
+                    </div> 
+                </div>
+            </div>
+            <div class="clearfix"></div> 
+            <!-- DEMON -->
+
+            <div class="wrapper">
+            <div id="tab_demons" class="tab_description">  
+                    <div class="description_content">
+                       
+                        <div class="content_text_wrapper">
+                                <div class="black" ></div>
+                                        <video class="video1" nocontrols autoplay muted loop>
+                                            <source src="video\doom_4.mp4" type="video/mp4">
+                                        </video>
+                                <div class="content">
+                                        <h2 class="title">2. демони</h2>
+                                        <div class="content_text" style="font-size: 30px;">
+                                            Демони - це ті з ким ви будете стикатись найчастіше.
+                                            <p class="demons_text">Демони, Імп, Зомбі, Арахнотрон, Солдат, Солдат із щитом,
+                                            Горгулья, Механічний зомбі, Рижучий Манкубус, Втрачена душа, Туша,
+                                            Рицар пекла, Пліть, Какодемон, Кіберманкубус, Елементаль болю, 
+                                            Ревенант, Пінки, Примара, Лицар жаху, Мародер, Барон пекла, Архвіль, Тиран</p>
+                                        </div>
+                                </div>
+                        </div>
+                    </div> 
+                </div>
+            </div>
+            <div class="clearfix"></div> 
+            <!-- GUNS -->
+
+            <div class="wrapper">
+            <div id="tab_guns" class="tab_description">  
+                    <div class="description_content">
+                       
+                        <div class="content_text_wrapper">
+                                <div class="black"></div>
+                                        <video class="video1" nocontrols autoplay muted loop >
+                                            <source src="video\doom_3.mp4" type="video/mp4">
+                                        </video>
+                                <div class="content">
+                                        <h2 class="title">3. Зброя</h2>
+                                        <div class="content_text" >
+                                            Зброя в Doom Eternal - це потужний інструмент для знищення демонів.
+                                            Ви можете модифікувати і удосконалювати своє озброєння за допомогою 
+                                            модів і бойових очок.
+                                            <div class="text_gun"><a class="link_gun" href="#">Більше інформації</a><img src="img\arrow4.svg" ></div>
+                                        </div>
+                                </div>
+                        </div>
+                    </div> 
+                </div>
+            </div>
+        
+
+    </div>     
+
+
+
+        <!-- <div class="sketchfab-embed-wrapper"> <iframe title="Doomslayer - Doom Eternal" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share width="640" height="480" src="https://sketchfab.com/models/53504985912b4993af4081eacf104812/embed?autostart=1&preload=1&ui_hint=0"> </iframe> </div>
+     -->
+   
+</main>
+
+
+    
+
+        <!-- FOOTER -->
+
+<footer class="footer">
+        <div class="foter_text">© 2023 Bethesda Softworks LLC, a ZeniMax Media company. Bethesda, Bethesda Softworks, ZeniMax and related logos are registered trademarks or trademarks of ZeniMax Media Inc. in the U.S. and/or other countries. DOOM, id, id Software, id Tech and related logos are registered trademarks or trademarks of id Software LLC in the U.S. and/or other countries. All Rights Reserved.</div>
+
+        
+        
+</footer>
+
+</body>
+</html>
